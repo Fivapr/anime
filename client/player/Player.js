551ab6dc5601aa9f69d53c770ebc4e9361996b03
Fivapr <nonprocrastinating@gmail.com>
2018-09-17 19:58:13 +0700

    some styles for subs

--- client/player/Player.js
+++ client/player/Player.js
@@ -19,16 +19,23 @@ class Player extends Component {
 
   render() {
     return (
-      <Fragment>
-        <video id="video" controls>
+      <div
+        style={{
+          display: 'inline-block',
+          position: 'relative',
+          width: 640,
+          height: 480,
+        }}
+      >
+        <video id="video" controls >
           <source
             src="http://cool.big-sword.ga/route/?m=3318651&r=bf304ccc729b4ffddf5bd5cd11237a57e3f6cd775184112644c95a1163d44cc3f373c54fd083e25f955d8c0a01e6bb302e3afb427727cf5e4539a036dbe560ab4dabcaa3cb23a9804c759fed0bfe08274602b12ac7314eaaedb336f17998f78b"
             type="video/mp4"
           />
-          <track label="Japanese" kind="subtitles" srcLang="jp" src={testSub} default style={{ zIndex: 1000000 }} />
+          <track label="Japanese" kind="subtitles" srcLang="jp" src={testSub} default />
         </video>
         <Subtitles currentSub={this.state.currentSub} />
-      </Fragment>
+      </div>
     );
   }
 }
--- client/player/Subtitles.js
+++ client/player/Subtitles.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledSubtitles = styled.div`
+  position: absolute;
+  bottom: 12%;
+  width: 90%;
+  margin: 0 5%;
+  text-align: center;
+  box-sizing: border-box;
+  background: rgba(0, 0, 0, 0.5);
+  font-family: Helvetica, Arial, sans-serif;
+  font-size: 24px;
+  text-shadow: #000000 1px 1px 0px;
+  color: #fc6;
+  line-height: 1.3;
+`;
 
 const Subtitles = ({ currentSub }) => (
-  <div>
+  <StyledSubtitles>
     {currentSub}
-  </div>
+  </StyledSubtitles>
 );
 
 export default Subtitles;
